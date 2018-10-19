a4bd1c4333239dc10626fadf64f16a3d9ce25b3f
Charles Zhang <gzcharleszhang@hotmail.com>
2018-10-19 10:48:03 -0400

    added more errors

--- src/server/error.js
+++ src/server/error.js
@@ -1,11 +1,29 @@
 class ServerError extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode = 500) {
     super();
     this.message = message;
-    this.statusCode = statusCode || 500;
+    this.statusCode = statusCode;
+  }
+}
+
+class AuthenticationError extends Error {
+  constructor(message, statusCode = 401) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+class InvalidParameterError extends Error {
+  constructor(param, statusCode = 500) {
+    super();
+    this.message = `Invalid Parameter: ${param}`;
+    this.statusCode = statusCode;
   }
 }
 
 module.exports = {
   ServerError,
+  AuthenticationError,
+  InvalidParameterError,
 };
